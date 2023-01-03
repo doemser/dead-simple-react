49dfa9fdf27ad6c67e05458d0f6b96dac85ab4cb
Dominik Hautau <dominikhautau@gmail.com>
2023-01-03 07:30:51 +0100

    another little correction to conways game of life ts

--- examples/games/games-conways-game-of-life-ts/src/index.tsx
+++ examples/games/games-conways-game-of-life-ts/src/index.tsx
@@ -4,7 +4,7 @@ import { createRoot } from "react-dom/client";
 import App from "./App";
 
 const rootElement = document.getElementById("root");
-const root = createRoot(rootElement);
+const root = createRoot(rootElement!);
 
 root.render(
   <StrictMode>
